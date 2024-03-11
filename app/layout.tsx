e51b11c543577f7df71630afc500bf753921bd41
André Navarro <andre_farao123@hotmail.com>
2024-03-11 19:11:04 -0300

    Add home page and card flip

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={myFont.className}>{children}</body>
+      <body className={myFont.className}>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,66 @@
+'use client';
+import './globals.css';
+
 import Link from 'next/link';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+
+import creaseSvg from './assets/letter-crease.svg';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <section className="flex flex-col items-center justify-center bg-tertiary rounded-3xl border-8 border-grey lg:w-letter aspect-video">
-        <h1 className="text-center lg:text-6xl text-primary text-stroke-1 mb-8">
-          Bubi&apos;s
-          <br />
-          Birthday Card
-        </h1>
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [isAnimating, setIsAnimating] = useState(false);
 
-        <Link passHref legacyBehavior href="/backface">
-          <button className="py-3 px-8 bg-secondary text-3xl text-white flex justify-center items-center rounded-lg">
+  const handleFlip = () => {
+    if (!isAnimating) {
+      setIsAnimating(true);
+      setIsFlipped(state => !state);
+    }
+  };
+
+  return (
+    <div className="flip-card">
+      <motion.div
+        className="flip-card-inner bg-tertiary rounded-3xl border-8 border-grey lg:w-letter aspect-video pb-1"
+        initial={false}
+        animate={{
+          rotateY: isFlipped ? 180 : 360,
+        }}
+        onAnimationComplete={() => setIsAnimating(false)}
+        transition={{
+          duration: 0.6,
+        }}
+      >
+        <div className="flip-card-front h-full w-full flex justify-center items-center flex-col">
+          <h1 className="text-center lg:text-6xl text-primary text-stroke-1 mb-8">
+            Bubi&apos;s
+            <br />
+            Birthday Card
+          </h1>
+
+          <button
+            onClick={handleFlip}
+            className="py-3 px-8 bg-secondary text-3xl text-white flex justify-center items-center rounded-lg"
+          >
             Flip
           </button>
-        </Link>
-      </section>
-    </main>
+        </div>
+
+        <div className="flip-card-back h-full w-full flex justify-center items-center flex-col">
+          <Image
+            src={creaseSvg}
+            alt="Letter crease"
+            className="w-full h-full"
+          />
+
+          <Link passHref legacyBehavior href="/gallery">
+            <button className="py-3 px-8 bg-secondary text-3xl text-white flex justify-center items-center rounded-lg absolute">
+              Open
+            </button>
+          </Link>
+        </div>
+      </motion.div>
+    </div>
   );
 }
